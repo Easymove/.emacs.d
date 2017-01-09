ab5aaab5af457f4262637e056c1e053c7ffd8b3b
easymove <easymove@localhost.localdomain>
2017-01-09 22:40:12 +0200

    Some magic about fasls and sbcl path detection.

--- inits/init-common-lisp.el
+++ inits/init-common-lisp.el
@@ -6,11 +6,36 @@
 (eval-after-load "auto-complete"
    '(add-to-list 'ac-modes 'slime-repl-mode))
 
+(defun shell-command-to-list (command-str &optional arg)
+  (let ((split-char (or arg " ")))
+    (remove-if
+     (lambda (s) (= (length s) 0))
+     (reduce
+      #'append
+      (mapcar (lambda (s) (split-string s split-char))
+              (split-string (shell-command-to-string command-str) "\n"))))))
+
+(defun find-sbcl ()
+  (interactive)
+  (block tmp
+    (dolist (str (shell-command-to-list "whereis sbcl"))
+      (when (string-match-p (regexp-quote "bin") str)
+        (return-from tmp str)))))
+
+;;; TODO: optimize this
+(defun find-ql ()
+  (interactive)
+  (block tmp
+    (dolist (str (shell-command-to-list "find / -name quicklisp.lisp"))
+      (when (string-match-p (regexp-quote "quicklisp.lisp") str)
+        (return-from tmp str)))))
+
+;;; redirecting fasls
+(make-directory "/tmp/slime-fasls/" t)
+(setq slime-compile-file-options '(:fasl-directory "/tmp/slime-fasls/"))
+
 ;; set lisp interpreter
-
-(setq inferior-lisp-program "/usr/bin/sbcl --load /home/easymove/libs-n-bins/quicklisp.lisp")
-
-(setq *FASL-DIRECTORY* "~/.fasls.d/")
+(setq inferior-lisp-program (format "%s --load /home/easymove/libs-n-bins/quicklisp.lisp" (find-sbcl)))
 
 (require 'slime-autoloads)
 (setq slime-contribs '(slime-fancy))
@@ -19,9 +44,3 @@
 (modify-syntax-entry ?- "w" lisp-mode-syntax-table)
 
 (provide 'init-common-lisp)
-
-;; (setf asdf:*central-registry*
-;;        (list* '*default-pathname-defaults*
-;;               #p"/home/me/cl/systems/"
-;;               #p"/usr/share/common-lisp/systems/"
-;;               asdf:*central-registry*))
